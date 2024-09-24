71b0c5f8fd192c322c4f8f139368e1e7374b901f
demiurges-tech <cochetyvesmarie@gmail.com>
2024-09-24 22:17:20 +0200

    version fonctionnelle VPC,SUBNET

--- data.tf
+++ data.tf
@@ -1,17 +1,2 @@
 data "aws_availability_zones" "available" {}
 
-data "aws_ami" "almalinux" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["*var.image-*"]
-  }
-
-  filter {
-    name   = "architecture"
-    values = ["x86_64"]
-  }
-
-}
-
