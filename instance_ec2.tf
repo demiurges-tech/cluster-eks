95173e8af5393171f51887623df317df2a8798c9
demiurges-tech <cochetyvesmarie@gmail.com>
2024-10-02 18:46:50 +0200

    ajout du module ec2

--- instance_ec2.tf
+++ instance_ec2.tf
@@ -1,8 +1,10 @@
-#resource "aws_instance" "ec2" {
-#  ami           = data.aws_ami..id
-#  instance_type = "t3.micro"
+module "ec2-instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.7.0"
 
-#  tags = {
-#    Name = "HelloWorld"
-#  }
-#}
+  name           = "my-ec2-instance"
+  instance_type  = "t2.micro"
+  ami            = data.aws_ami.almalinux.id
+  #key_name       = "my-key"
+}
+
