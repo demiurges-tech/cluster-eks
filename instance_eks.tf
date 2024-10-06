bc1bba56f1f4c08d72771745bde85866e0a9072a
demiurges-tech <cochetyvesmarie@gmail.com>
2024-10-06 21:03:43 +0200

    eks ok

--- instance_eks.tf
+++ instance_eks.tf
@@ -3,32 +3,32 @@ module "eks" {
   version = "20.24.3"
 
 
-  count          = length(data.aws_availability_zones.available.names)
+  #count           = length(data.aws_availability_zones.available.names)
   cluster_name    = "ycochet_eks"
   cluster_version = "1.30"
 
-  iam_role_name = "EKS_Students"
-  cluster_endpoint_public_access  = true
-  create_cloudwatch_log_group = false
-
+  iam_role_name                            = "EKS_Students"
+  cluster_endpoint_public_access           = true
+  create_cloudwatch_log_group              = false
+  enable_cluster_creator_admin_permissions = true
 
   cluster_addons = {
-    coredns                = {}
+    coredns = {}
     #eks-pod-identity-agent = {}
-    kube-proxy             = {}
-    vpc-cni                = {}
+    kube-proxy = {}
+    vpc-cni    = {}
   }
 
   vpc_id                   = aws_vpc.ycochet_vpc.id
-  subnet_ids               = ["${element(aws_subnet.ycochet_pub.*.id, count.index)}"]
-  control_plane_subnet_ids = ["${element(aws_subnet.ycochet_priv.*.id, count.index)}"]
+  subnet_ids               = ["${element(aws_subnet.ycochet_pub.*.id, 0)}", "${element(aws_subnet.ycochet_pub.*.id, 1)}"]
+  control_plane_subnet_ids = ["${element(aws_subnet.ycochet_priv.*.id, 0)}", "${element(aws_subnet.ycochet_priv.*.id, 1)}"]
 
   # EKS Managed Node Group
 
   eks_managed_node_groups = {
     example = {
       ami_type       = "CUSTOM"
-      ami_id = data.aws_ami.ubuntu.id
+      ami_id         = data.aws_ami.ubuntu.id
       instance_types = ["t2.micro"]
 
       min_size     = 1
@@ -40,9 +40,9 @@ module "eks" {
   }
 
 
- enable_cluster_creator_admin_permissions = true
- 
-tags = {
+
+
+  tags = {
     Environment = "dev"
     Terraform   = "true"
   }
--- security_key.tf
+++ security_key.tf
@@ -1,5 +1,5 @@
 resource "aws_key_pair" "ycochet_public_key" {
   key_name   = "key_public_ec2"
   public_key = file("~/.ssh/key_aws.pem.pub")
-#ssh-keygen -t rsa -b 2048 -m PEM -f ~/.ssh/key_aws.pem
+  #ssh-keygen -t rsa -b 2048 -m PEM -f ~/.ssh/key_aws.pem
 }
