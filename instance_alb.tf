b59f40f8596652c1e439255c51dfeb034b5aadc6
demiurges-tech <cochetyvesmarie@gmail.com>
2024-10-09 18:02:13 +0200

    alb

---
+++ instance_alb.tf
@@ -0,0 +1,14 @@
+resource "aws_lb" "alb" {
+  name               = "ycochetalb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.security_group_admin.id}"]
+  subnets            = [for subnet in aws_subnet.ycochet_pub : subnet.id]
+
+  enable_deletion_protection = false
+
+
+  tags = {
+    Environment = "production"
+  }
+}
