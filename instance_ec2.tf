7317b843133e82792c42c29968cc843c6643e272
demiurges-tech <68774629+demiurges-tech@users.noreply.github.com>
2024-10-09 18:59:48 +0200

    ec2 resource (#7)

--- instance_ec2.tf
+++ instance_ec2.tf
@@ -1,14 +1,14 @@
-module "ec2-instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "5.7.0"
-
+resource "aws_instance" "ycochet_ec2" {
   availability_zone      = data.aws_availability_zones.available.names[0]
   subnet_id              = element(aws_subnet.ycochet_pub.*.id, 0)
   vpc_security_group_ids = ["${aws_security_group.security_group_admin.id}"]
-  name                   = "ycochet_ec2_manage"
-  instance_type          = "t2.micro"
-  ami                    = data.aws_ami.ubuntu.id
 
-  key_name = "key_public_ec2"
+  instance_type = "t2.micro"
+  ami           = data.aws_ami.ubuntu.id
+  key_name      = "key_public_ec2"
+  tags = {
+    Name = "ycochet_ec2"
+  }
+
 }
 
