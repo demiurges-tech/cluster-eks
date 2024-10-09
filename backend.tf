b2f3441ea6dec495a72df7a181e0e4917f225160
demiurges-tech <cochetyvesmarie@gmail.com>
2024-10-09 18:07:58 +0200

    mep

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "ycochet-state-bucket"
-    key            = "terraform.tfstate"
-    region         = "us-west-1"
+    bucket = "ycochet-state-bucket"
+    key    = "terraform.tfstate"
+    region = "us-west-1"
   }
 }
